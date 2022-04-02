098619f81f43107f72fa0d9b9aef378193d6068e
Rayan-Ald <rayan.alduini@gmail.com>
2022-04-02 21:02:13 +0200

    initial commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <v-btn
+        color="grey lighten-1"
+        flat
+        v-for="(t,index) in titles" :key="index" :style="{color : t.color}" @click="$router.push({path:t.path})">{{t.text}}</v-btn>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'NavBar',
+    props: ['titles']
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,40 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import {viruses} from "@/model";
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    viruses: viruses,
+    parts: [],
+    samples: [],
+    basket: [],
   },
   mutations: {
+    addPart(state, part) {
+      state.parts.push(part)
+    },
+
+    addVirus(state, virus) {
+      state.basket.push(virus)
+    },
+
+    setSample(state) {
+      state.basket.forEach(elem => {
+        state.samples.push(elem)
+      })
+    },
+
+    removeBasket(state) {
+      state.basket.splice(0)
+    }
+  },
+  getters: {
+    virus(state) { return state.virus },
+    parts(state) { return state.parts },
+    samples(state) { return state.samples },
+    basket(state) { return state.basket },
   },
   actions: {
   },
